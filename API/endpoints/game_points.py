1e75c15f0f22ff78de8bfe07192846de7d0d9494
Neveric <kerkker@gmail.com>
2023-04-21 12:03:54 +0400

    png

--- API/endpoints/game_points.py
+++ API/endpoints/game_points.py
@@ -15,7 +15,7 @@ async def root():
 
 @router.get("/card/{card_number}")
 async def card_test(card_number: int):
-    return FileResponse(f"./assets/cards/{card_number}.gif")
+    return FileResponse(f"./assets/cards/{card_number}.png")
 
 @router.get("/refresh_board")
 async def refresh_board():
